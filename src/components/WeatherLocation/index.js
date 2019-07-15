dec89c6788ff98530cb4dd9c015569da48233949
Rolando <rjuaniquina@edrans.com>
2019-07-15 18:14:01 -0300

    Get api weather data

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -11,6 +11,12 @@ import {
 } from '../../constants/weathers';
 import './styles.css';
 
+const location = "Buenos Aires,ar";
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 6,
     weatherState: SUN,
@@ -37,6 +43,12 @@ class WeatherLocation extends Component {
 
     handleUpdateClick = () => {
         console.log('Actualizado');
+        fetch(api_weather).then( response => {
+            return response.json();
+        }).then( data => {
+            console.log(data);
+            debugger;
+        });
         this.setState({
             city: 'Catamarca',
             data: data2
