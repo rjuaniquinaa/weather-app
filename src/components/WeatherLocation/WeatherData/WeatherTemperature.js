5fa197eead42806eda9c8ac5fbfc4a206c25db09
Rolando <rjuaniquina@edrans.com>
2019-07-17 18:14:21 -0300

    Mapping icons

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -2,22 +2,22 @@ import React from 'react';
 import WeatherIcons from 'react-weathericons';
 import {
     CLOUD,
-    CLOUDY,
     SUN,
     RAIN,
     SNOW,
-    WINDY,
+    THUNDER,
+    DRIZZLE,
 } from './../../../constants/weathers';
 import PropTypes from 'prop-types';
 import './styles.css';
 
 const icons = {
     [CLOUD]: 'cloud',
-    [CLOUDY]: 'cloudy',
     [SUN]: 'day-sunny',
     [RAIN]: 'rain',
     [SNOW]: 'snow',
-    [WINDY]: 'windy'
+    [THUNDER]: 'day-thunderstorm',
+    [DRIZZLE]: 'day-showers',
 };
 
 const getWeatherIcon = weatherState => {
--- src/services/transformWeather.js
+++ src/services/transformWeather.js
@@ -1,25 +1,41 @@
 import convert from 'convert-units';
 import {
-    // CLOUD,
-    // CLOUDY,
+    CLOUD,
     SUN,
-    /* RAIN,
+    RAIN,
     SNOW,
-    WINDY, */
+    THUNDER,
+    DRIZZLE,
 } from '../constants/weathers';
 
 const getTemp = kelvin => {
     return Number(convert(kelvin).from('K').to('C').toFixed(2));
 }
 
-const getWeatherState = weatherData => {
-    return SUN;
+const getWeatherState = weather => {
+    const { id } = weather;
+
+    if (id < 300) {
+        return THUNDER;
+    } else if (id < 400) {
+        return DRIZZLE;
+    } else if (id < 500) {
+        return RAIN;
+    } else if (id < 600) {
+        return SUN;
+    } else if (id < 700) {
+        return SNOW;
+    } else if (id === 800) {
+        return SUN;
+    } else {
+        return CLOUD;
+    }
 }
 
 const transformWeather = weatherData => {
     const { humidity, temp } = weatherData.main;
     const { speed } = weatherData.wind;
-    const weatherState = getWeatherState(weatherData);
+    const weatherState = getWeatherState(weatherData.weather[0]);
     const temperature = getTemp(temp);
 
     const data = {
